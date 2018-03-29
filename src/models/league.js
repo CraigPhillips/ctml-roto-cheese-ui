3b02a56eeb71facb842b32786e7ada597d11dffb
Craig Phillips <craig.phillips@frozenexports.net>
2018-03-29 22:02:14 +0000

    added week score retrieval

--- src/models/league.js
+++ src/models/league.js
@@ -13,6 +13,17 @@ const League = {
         .keys(result)
         .filter(k => k !== 'currentWeek')
         .forEach(k => { League.teams[k] = result[k]; });
+    })
+    .then(() => m.request({
+      method: 'GET',
+      url: `data/scores-week-${League.currentWeek}.json`,
+    }))
+    .then((result) => {
+      console.log(result);
+      Object
+        .keys(result)
+        .filter(k => k !== 'weekNumber')
+        .forEach(k => { League.teams[k].scores = result[k]; });
     });
   },
 
